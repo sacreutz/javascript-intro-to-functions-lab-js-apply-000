e814e7bff5826e06e26017403c5dd8499ab450e4
sacreutz <sacreutz@gmail.com>
2019-04-19 00:54:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   if (string.toUpperCase === string){
-    return 'YES INDEED!'
+    return "YES INDEED!"
   } else 
   if (string.toLowerCase === string){
     return 'I can\'t hear you!'
